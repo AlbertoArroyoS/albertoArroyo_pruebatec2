a85d8a4864c25117b17e17436dcbbb0692517c43
Alberto Arroyo <albertoarroyo@hotmail.es>
2025-01-02 11:21:49 +0100

    Actualizado persistence, creada capa logica con entidades y enum

---
+++ PruebaTecnica2/src/main/java/com/hackaboss/pruebatecnica2/logica/Ciudadano.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hackaboss.pruebatecnica2.logica;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Alberto
+ */
+@Table(name = "ciudadanos")
+@Entity
+public class Ciudadano {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String email;
+
+    public Ciudadano() {
+    }
+
+    public Ciudadano(String nombre, String email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+}
---
+++ PruebaTecnica2/src/main/java/com/hackaboss/pruebatecnica2/logica/Turno.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hackaboss.pruebatecnica2.logica;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Alberto
+ */
+@Entity
+public class Turno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int numero;
+
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+
+    private String descripcion;
+
+    @Enumerated(EnumType.STRING)
+    private Estado estado;
+
+    @ManyToOne
+    private Ciudadano ciudadano;
+
+    public Turno() {
+    }
+
+    public Turno(int numero, Date fecha, String descripcion, Estado estado, Ciudadano ciudadano) {
+        this.numero = numero;
+        this.fecha = fecha;
+        this.descripcion = descripcion;
+        this.estado = estado;
+        this.ciudadano = ciudadano;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public Ciudadano getCiudadano() {
+        return ciudadano;
+    }
+
+    public void setCiudadano(Ciudadano ciudadano) {
+        this.ciudadano = ciudadano;
+    }
+
+    
+}
